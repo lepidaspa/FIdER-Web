e391898eb4cbfc6eadb07a1bbc2a7c0b356bdb25
Marco Montanari <sirmmo@gmail.com>
2012-05-25 16:31:01 +0200

    static serving corrected

--- FiderWeb/urls.py
+++ FiderWeb/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^static/', 'django.views.static.serve', {
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': settings.MEDIA_ROOT,
         }),
     
