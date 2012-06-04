7ca1ec3136518c17dd206962d327920dd9041c8a
Marco Montanari <sirmmo@gmail.com>
2012-06-04 19:33:57 +0200

    mods

--- FiderWeb/urls.py
+++ FiderWeb/urls.py
@@ -19,6 +19,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': settings.MEDIA_ROOT,
+            'allow_indexes':True
         }),
     
          
